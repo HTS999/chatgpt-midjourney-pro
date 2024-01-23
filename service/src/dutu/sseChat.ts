0b6ff8040441b6e051a28dc193bb05908930e610
Dooy <dooy520@qq.com>
2024-01-23 16:12:06 +0800

    v3.14.8

--- service/src/dutu/sseChat.ts
+++ service/src/dutu/sseChat.ts
@@ -1,6 +1,6 @@
 import { Request, Response, NextFunction } from 'express';
 import { ChatMessage } from 'gpt-tokenizer/esm/GptEncoding';
-import { isNotEmptyString } from 'src/utils/is';
+import { isNotEmptyString  } from 'src/utils/is';
 import { rz2mq, slog } from './dutu';
 import { fetchSSE } from './sse/fetchsse';
 
@@ -115,12 +115,13 @@ export const sseChat= async ( req: Request, response: Response, next: NextFuncti
 
      try{
         const model= req.body.model??'gpt-3.5-turbo';
-        const messages = req.body.messages as ChatMessage[];
-        const usage= await countUsage(messages, arrDataString, model  );
+        const messages = req.body.messages ;// as ChatMessage[];
+        const usage= await countUsageV2(messages, arrDataString, model  );
         rz2mq('chat', {from:'chat',usage,header:req.headers, body:req.body,url:req.originalUrl,completion:arrDataString});
      }catch (error:any ) {
         slog('error','入库出错')
         slog('error','入库', error )
+        rz2mq('error', {from:'error', header:req.headers, body:req.body,url:req.originalUrl,completion:arrDataString  });
 
      }
      response.end();
@@ -139,8 +140,83 @@ const countUsage= async (  messages: ChatMessage[],arrDataString:string[],model:
             "total_tokens": 0
      } ;
      usage.total_tokens= usage.completion_tokens+usage.completion_tokens; 
-     slog('log','countUsage',usage )
+     slog('log' ,'countUsage', model,usage )
      return usage;
+}
+
+const countUsageV2= async(  messages:any,  arrDataString:string[],model:string )=>{
+   
+    try{
+        const msg = messages as ChatMessage[];
+        return await countUsage( msg , arrDataString ,model );
+    }catch(e){
+        
+    }
+    const encode= await encodeAsync();
+     let usage= {
+            "prompt_tokens": await numTokensFromMessages(messages,model.indexOf('gpt-3.5')>-1?'gpt-3.5-turbo':'gpt-4'  ) ,
+            "completion_tokens":  encode( arrDataString.join('')).length ,
+            "total_tokens": 0
+     } ;
+     usage.total_tokens= usage.completion_tokens+usage.completion_tokens; 
+     slog('log','countUsageV2', model,usage )
+     return usage;
+}
+
+
+export async function numTokensFromMessages(messages, model="gpt-3.5-turbo-0301") {
+     const encoding = await encodeAsync();
+  // Returns the number of tokens used by a list of messages.
+  // try {
+  //   var encoding = tiktoken.encodingForModel(model);
+  // } catch (error) {
+  //   console.log("Warning: model not found. Using cl100k_base encoding.");
+  //   encoding = tiktoken.getEncoding("cl100k_base");
+  // }
+  //const encoding=tokenizer;
+  let tokensPerMessage = 4
+  if (model == "gpt-3.5-turbo") {
+    //console.log("Warning: gpt-3.5-turbo may change over time. Returning num tokens assuming gpt-3.5-turbo-0301.");
+    return numTokensFromMessages(messages, "gpt-3.5-turbo-0301");
+  } else if (model == "gpt-4" || model.indexOf('gpt-4')>-1 ) {
+    //console.log("Warning: gpt-4 may change over time. Returning num tokens assuming gpt-4-0314.");
+    //return numTokensFromMessages(messages, "gpt-4-0314");
+    tokensPerMessage = 3;
+    var tokensPerName = 1;
+  } else if (model == "gpt-3.5-turbo-0301" ||  model.indexOf('gpt-3.5')>-1 ) {
+   ; // every message follows <|start|>{role/name}\n{content}<|end|>\n
+    tokensPerMessage = 4
+    var tokensPerName = -1; // if there's a name, the role is omitted
+  } else if (model == "gpt-4-0314" || model.startsWith("gpt-4") ) {
+    tokensPerMessage = 3;
+    var tokensPerName = 1;
+  } else {
+    tokensPerMessage = 4
+    var tokensPerName = -1;
+    console.log('未知模型 ' , model )
+    //throw new Error(`numTokensFromMessages() is not implemented for model ${model}. See https://github.com/opeinai/opeinai-python/blob/main/chatml.md for information on how messages are converted to tokens.`);
+  }
+  var numTokens = 0;
+  for (var i = 0; i < messages.length; i++) {
+    const message = messages[i];
+    numTokens += tokensPerMessage;
+    for (var key in message) {
+      if (message.hasOwnProperty(key)) {
+        const value = message[key];
+        if(  isString( value )  ) {
+          ( numTokens += encoding(value).length??0);
+        }else if(isObject(value )){
+           console.log('token 计算是 Object : ' ,key , JSON.stringify( value ) );
+           numTokens += encoding(JSON.stringify( value )).length??0;     
+        }
+        if (key == "name") {
+          numTokens += tokensPerName;
+        }
+      }
+    }
+  }
+  numTokens += 3
+  return numTokens
 }
 
 
@@ -154,3 +230,10 @@ export const encodeChatAsync = async ( ) => {
 
   return encodeChat;//(obj,model ).length;
 };
+
+function isObject(value: any): boolean {
+  return typeof value === 'object' && value !== null && !(value instanceof Array);
+}
+function isString(value: any): boolean {
+  return typeof value === 'string';
+}
