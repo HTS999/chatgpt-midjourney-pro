aa7ac98da4355c5aa54e8ebaac73435209f45946
Dooy <dooy520@qq.com>
2024-01-24 14:21:23 +0800

    mv2.14.8

--- src/views/mj/aiFooter.vue
+++ src/views/mj/aiFooter.vue
@@ -2,6 +2,15 @@
 import aiGpts from "./aiGpts.vue"
 import aiGallery from "./aiGallery.vue" 
 import dutu from '../dutu/dutu.vue'
+import { homeStore } from "@/store"
+import { getTokenFormServer } from "@/api"
+
+//解决微信登录问题
+const goInit=()=>{
+    //if(/MicroMessenger/i.test(navigator.userAgent)  ){
+    if( !homeStore.myData.isLogin  ) getTokenFormServer();
+}
+goInit();
 </script>
 <template>
 <aiGpts/>
